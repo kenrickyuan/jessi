5a15a4910081fffd9dad4b9003538b95d43f6ad8
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-21 21:54:09 +0100

    removed a submit button bevel, create and edit events have appropriate submit button text, autocomplete off for date input and poll question/question inputs

--- app/javascript/plugins/choices.js
+++ app/javascript/plugins/choices.js
@@ -1,24 +1,24 @@
-let n = 3
+let n = 3;
 
 const addNewButton = () => {
-  console.log(n)
+  console.log(n);
   const question =
     `<div class="form-group">` +
     `<label for='${n}'>Option ${n}</label>` +
-    `<input name="choices[]" type="text" id="${n}" class="form-control form-group" />` +
+    `<input name="choices[]" type="text" id="${n}" class="form-control form-group" autocomplete="off" />` +
     `</div>` +
     `<hr style="border-width: 4px;" class="m-0">`;
-  n += 1
+  n += 1;
   const formElement = document.getElementById("choices");
 
-  formElement.insertAdjacentHTML('beforeend', question);
-}
+  formElement.insertAdjacentHTML("beforeend", question);
+};
 
 const initMultipleChoices = () => {
-  const addChoiceBtn = document.querySelector('.add-choice-btn');
-  console.log(addChoiceBtn)
+  const addChoiceBtn = document.querySelector(".add-choice-btn");
+  console.log(addChoiceBtn);
   if (addChoiceBtn == null) return;
-  addChoiceBtn.addEventListener('click', addNewButton);
+  addChoiceBtn.addEventListener("click", addNewButton);
 };
 
 export default initMultipleChoices;
